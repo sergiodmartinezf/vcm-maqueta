343a7c2b8115e47bb28f4c143a517e498eb8d31c
Sergio Martínez <104039933+sergiodmartinezf@users.noreply.github.com>
2024-02-12 15:57:21 -0300

    reestructuración y uso de base de datos relacional 2

--- maqueta_bdd_no_relacional/src/database.js
+++ maqueta_bdd_no_relacional/src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-// Conexión a base de datos
+// Conexión a base de datos 
 const database = mongoose.connect('mongodb://localhost:27017/datos_vcm')
     .then(() => console.log('Base de Datos Conectada'))
     .catch(error => console.log('Hubo un Error de Conexión a la Base de Datos:'+'\n'+error));
